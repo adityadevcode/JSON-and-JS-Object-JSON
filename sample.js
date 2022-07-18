4bdfa909aea75588823d32babc8d2ab23084b431
addycoderepo <adityagitcode@gmail.com>
2022-07-18 12:04:00 -0400

    json and json obj

---
+++ sample.js
@@ -0,0 +1,28 @@
+/*
+let history ={
+    firstName:"sum",
+    lastName:"sum",
+    age:38,
+    place:function(){
+     console.log("us")
+    },
+    city:function(){
+        console.log("det")
+    }
+}
+ 
+history;
+history.place;
+history.city();
+history.age;
+*/
+
+// json obj
+{
+
+    "name";"some",
+        "place";"key",
+    "age";20,
+        "city";"det"
+}
+
